ca82c5c97eb1d604106e58f15ef186e17467982b
judeal3000 <103972605+kosukeminoru@users.noreply.github.com>
2022-10-03 12:18:53 -0500

    cxx encorperated

--- src/blockchain/transactions.rs
+++ src/blockchain/transactions.rs
@@ -1,6 +1,5 @@
-use libp2p::core::identity::secp256k1::Keypair;
-use libp2p::core::identity::secp256k1::PublicKey;
-use libp2p::core::identity::PublicKey as PubKey;
+use libp2p::core::identity::Keypair;
+use libp2p::core::identity::PublicKey;
 use libp2p::PeerId;
 use serde::{Deserialize, Serialize};
 use serde_big_array::BigArray;
@@ -16,12 +15,12 @@ pub struct Transaction {
 }
 impl Transaction {
     pub fn new(k: Keypair, t: &PublicKey, v: f32, n: u32) -> Transaction {
-        let bytes = k.public().encode();
-        let d = TxData::new(bytes, t.encode(), v, n);
+        let bytes = k.public().to_protobuf_encoding();
+        let d = TxData::new(bytes, t.to_protobuf_encoding(), v, n);
         let hash_d = d.hash();
         Transaction {
             data: d,
-            signature: k.secret().sign_hash(&*hash_d.as_bytes()).unwrap(),
+            signature: k.sign(&*hash_d.as_bytes()).unwrap(),
         }
     }
     pub fn hash(&self) -> String {
@@ -32,17 +31,15 @@ impl Transaction {
         result
     }
     pub fn verify_transaction_sig(&self) -> bool {
-        let pubkey: PublicKey = PublicKey::decode(&self.data.sender).unwrap();
+        let pubkey: PublicKey = PublicKey::from_protobuf_encoding(&self.data.sender).unwrap();
         let v_data = self.data.hash();
         let msg: &[u8] = &*v_data.as_bytes();
         let sig: &[u8] = &self.signature;
-        pubkey.verify_hash(msg, sig)
+        pubkey.verify(msg, sig)
     }
     pub fn verify_value(&self, values: &ValueList) -> bool {
-        let pubkey: PublicKey = PublicKey::decode(&self.data.sender).unwrap();
-        let acc = values
-            .account(&PeerId::from_public_key(&PubKey::Secp256k1(pubkey)))
-            .unwrap();
+        let pubkey: PublicKey = PublicKey::from_protobuf_encoding(&self.data.sender).unwrap();
+        let acc = values.account(&PeerId::from_public_key(&pubkey)).unwrap();
         if acc.value <= self.data.value && acc.nonce == self.data.nonce {
             return true;
         } else {
@@ -53,15 +50,13 @@ impl Transaction {
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct TxData {
-    #[serde(with = "BigArray")]
-    pub sender: [u8; 33],
-    #[serde(with = "BigArray")]
-    pub recepient: [u8; 33],
+    pub sender: Vec<u8>,
+    pub recepient: Vec<u8>,
     pub value: f32,
     pub nonce: u32,
 }
 impl TxData {
-    pub fn new(from: [u8; 33], to: [u8; 33], val: f32, once: u32) -> TxData {
+    pub fn new(from: Vec<u8>, to: Vec<u8>, val: f32, once: u32) -> TxData {
         TxData {
             sender: from,
             recepient: to,
--- src/protocol/core.rs
+++ src/protocol/core.rs
@@ -3,10 +3,21 @@ use super::behaviour::Event;
 use crate::{
     blockchain::{block::Block, transactions::Transaction},
     db::db,
-    protocol::{behaviour::MyBehaviour, gossipsub, kademlia, rr, transport},
+    ffi::{self, GameSend},
+    peers,
+    protocol::{
+        behaviour::MyBehaviour,
+        gossipsub, kademlia,
+        rr::{self, BlockRequest, BlockResponse},
+        transport,
+    },
+    structs::Accounts,
+    MultiBuf,
 };
+use async_std::io::{self};
 use bytemuck::__core::iter;
-use crossbeam_channel::{Receiver, Sender};
+use crossbeam_channel::{unbounded, Receiver, Sender};
+use futures::AsyncBufReadExt;
 use futures::{executor::block_on, StreamExt};
 use libp2p::{
     futures::select,
@@ -16,7 +27,9 @@ use libp2p::{
     kad::record::store::MemoryStore,
     kad::Kademlia,
     mdns::{Mdns, MdnsConfig, MdnsEvent},
-    request_response::{ProtocolSupport, RequestResponse},
+    request_response::{
+        ProtocolSupport, RequestResponse, RequestResponseEvent, RequestResponseMessage,
+    },
     swarm::SwarmEvent,
     Multiaddr, PeerId, Swarm,
 };
@@ -63,35 +76,56 @@ pub async fn into_protocol(
         swarm.listen_on("/ip4/10.150.99.25/tcp/65427".parse()?)?;
         swarm.listen_on("/ip6/::0/tcp/0".parse()?)?;
         swarm.behaviour_mut().gossipsub.subscribe(&t).unwrap();
-        swarm
-            .behaviour_mut()
-            .gossipsub
-            .publish(
-                b.clone(),
-                db::serialize(&Block::default()).expect("serde errir"),
-            )
-            .expect("pub");
+        swarm.behaviour_mut().gossipsub.subscribe(&b).unwrap();
     } else {
         swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
         swarm.listen_on("/ip6/::0/tcp/0".parse()?)?;
         swarm.behaviour_mut().gossipsub.subscribe(&b).unwrap();
-        if let identity::Keypair::Secp256k1(key) = local_key {
-            swarm
-                .behaviour_mut()
-                .gossipsub
-                .publish(
-                    t.clone(),
-                    db::serialize(&Transaction::new(key.clone(), key.public(), 1.0, 1))
-                        .expect("serde errir"),
-                )
-                .expect("pub");
-        }
+        swarm.behaviour_mut().gossipsub.subscribe(&t).unwrap();
     }
     //swarm.listen_on("/ip4/192.168.1.197/tcp/54005".parse()?)?;
     swarm = kademlia::boot(swarm);
-
+    let mut stdin = io::BufReader::new(io::stdin()).lines().fuse();
+    cxx();
     loop {
         select! {
+            line = stdin.select_next_some() => {
+                println!("{:?}", line);
+                match line {
+                    Result::Ok(a) => {
+                        if a == "1".to_string() {
+                            swarm
+                                .behaviour_mut()
+                                .gossipsub
+                                .publish(
+                                    b.clone(),
+                                    db::serialize(&Block::default()).expect("serde errir"),
+                                )
+                                .expect("pub");
+                        } else if a == "2".to_string() {
+                            let private = identity::Keypair::from_protobuf_encoding(&peers::P1KEY).expect("Decoding Error");
+                            let  peerid = PeerId::from(private.public());
+                            swarm.behaviour_mut().request.send_request(&peerid, BlockRequest());
+                            let keyz = &identity::Keypair::generate_secp256k1();
+                            swarm
+                                .behaviour_mut()
+                                .gossipsub
+                                .publish(
+                                    t.clone(),
+                                    db::serialize(&Transaction::new(
+                                        local_key.clone(),
+                                        &keyz.public(),
+                                        1.0,
+                                        1,
+                                    ))
+                                    .expect("serde errir"),
+                                )
+                                .expect("pub");
+                        }
+                    }
+                    _ => println!("empy"),
+                }
+            },
             event = swarm.select_next_some() => match event {
                 SwarmEvent::NewListenAddr { address, .. } => {
                     println!("Listening in {:?}", address);
@@ -122,8 +156,59 @@ pub async fn into_protocol(
                     }
 
                 }
+                SwarmEvent::Behaviour(Event::RequestResponse(RequestResponseEvent::Message { peer: _, message })) => match message {
+                //Request
+                // We can go through this. Pretty much the goal is to remember the most frequent block.
+                //It might be good the create your own algorithm for this. Keep in mind it is a bit funky.
+                RequestResponseMessage::Request {
+                    request_id: _,
+                    request: _,
+                    channel,
+                } => {
+                    swarm.behaviour_mut().request
+                        .send_response(
+                            channel,
+                            BlockResponse(Accounts::new(), Block::default()),
+                        )
+                        .expect("response error");
+                }
+                //response
+                RequestResponseMessage::Response {
+                    request_id: _,
+                    response,
+                } => {
+                    let BlockResponse(accounts, block) = response;
+                    println!("{:?}", accounts);
+                    println!("{:?}", block);
+                }
+            },
                 _ => (),
             }
+
         }
     }
+}
+
+pub fn cxx() {
+    let client = ffi::new_blobstore_client();
+
+    // Upload a blob.
+    let chunks = vec![b"fearless".to_vec(), b"concurrency".to_vec()];
+    let (game_sender, backend_reciever) = unbounded::<GameSend>();
+    let mut buf = MultiBuf {
+        senda: game_sender,
+        chunks,
+        pos: 0,
+    };
+    let blobid = client.put(&mut buf);
+    println!("blobid = {}", blobid);
+
+    // Add a tag.
+    client.tag(blobid, "rust");
+
+    // Read back the tags.
+    let metadata = client.metadata(blobid);
+    println!("tags = {:?}", metadata.tags);
+
+    println!("{:?}", backend_reciever.try_recv());
 }
